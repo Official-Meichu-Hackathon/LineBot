e0e021c83fd497407ecea621d3c7042f4d24fb75
r1407p <buszter0403@gmail.com>
2023-10-13 02:12:27 +0800

    faet: finish line bot

--- linebot_2023/mainbot/admin_ids.py
+++
@@ -1,6 +0,0 @@
-admin_ids = [
-    "Ue6dccfd85e04a611e212c7b05423d44d",
-    "U2af14accb39e02983fe5d1adcd9da8d3",
-    "U5db392f87f5f8de72b560d4a502dbb0d",
-    "U8aa025b8fdc622653288b13896fdbc3b"
-    ]
