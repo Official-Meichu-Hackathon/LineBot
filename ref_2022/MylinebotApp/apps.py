665d44a61e0023eecbce5e2fac037a02f2d54456
r1407p <buszter0403@gmail.com>
2023-10-13 02:23:05 +0800

    feat: tidy up

--- ref_2022/MylinebotApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MylinebotappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'MylinebotApp'
--- ref_2022/MylinebotApp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import include, re_path
-from . import views
-# 用來串接callback主程式
-urlpatterns = [
-    re_path('^callback/', views.callback),
-]
